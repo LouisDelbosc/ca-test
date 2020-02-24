fa49d45a70848b9426255a82a735748b66ce72a5
Louis Delbosc <louisdelbosc@gmail.com>
2020-02-24 15:42:35 +0100

    Fix suggestion bug

--- src/components/movieSuggestions.js
+++ src/components/movieSuggestions.js
@@ -1,16 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { MovieLink } from "./movieLink";
 import { movieFetch } from "../moviefetch";
 
 export function MovieSuggestions({ id }) {
   const [page, setPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(true);
   const [suggestions, setSuggestions] = useState([]);
+  const refId = useRef(id);
   useEffect(() => {
     const fetchData = async () => {
-      const json = await movieFetch(`/movie/${id}/recommendations`, { page });
-      if (json.results) {
-        setSuggestions(curr => [...curr, ...json.results]);
+      if (refId.current !== id) {
+        const json = await movieFetch(`/movie/${id}/recommendations`, {
+          page: 1
+        });
+        setMaxPage(page === json.total_pages);
+        if (json.results) {
+          setSuggestions(json.results);
+        }
+      } else {
+        const json = await movieFetch(`/movie/${id}/recommendations`, { page });
+        setMaxPage(page === json.total_pages);
+        if (json.results) {
+          setSuggestions(curr => [...curr, ...json.results]);
+        }
       }
+      refId.current = id;
     };
     fetchData();
   }, [id, page]);
@@ -19,14 +33,16 @@ export function MovieSuggestions({ id }) {
       Suggestions:
       <ul>
         {suggestions.map(movie => (
-          <li>
+          <li key={movie.id}>
             <MovieLink id={movie.id}>{movie.title}</MovieLink>
           </li>
         ))}
       </ul>
-      <button onClick={() => setPage(curr => curr + 1)}>
-        Plus de suggestions
-      </button>
+      {!maxPage ? (
+        <button onClick={() => setPage(curr => curr + 1)}>
+          Plus de suggestions
+        </button>
+      ) : null}
     </div>
   ) : (
     <div>Pas de suggestions</div>
